4b1188c1d9d07d0d1cd0157d96ad9ab303e6600c
Kozubskyi Denys <kozubskiy.denis@gmail.com>
2023-07-19 15:35:22 +0300

    v 0.1.20

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -4,8 +4,8 @@ import data from '../../data'
 const Info = () => {
 	return (
 		<div className="Info">
-			<div>Версія: 0.1.19</div>
-			<div>Оновлено: 18.07.2023</div>
+			<div>Версія: 0.1.20</div>
+			<div>Оновлено: 19.07.2023</div>
 			<div>Всього родичів: {data.length}</div>
 		</div>
 	)
