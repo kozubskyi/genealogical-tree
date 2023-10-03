f5c9a0b9e53fc8462d53899a0025a2f110cae229
Kozubskyi Denys <kozubskiy.denis@gmail.com>
2023-10-03 23:07:33 +0300

    оновив версію

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -4,8 +4,8 @@ import data from '../../data'
 const Info = () => {
 	return (
 		<div className="Info">
-			<div>Версія: 0.1.23</div>
-			<div>Оновлено: 30.09.2023</div>
+			<div>Версія: 0.1.24</div>
+			<div>Оновлено: 03.10.2023</div>
 			<div>Всього родичів: {data.length}</div>
 		</div>
 	)
