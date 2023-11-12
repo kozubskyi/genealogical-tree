646d201dea2af3ef015e6b36de335b3a7cb716f9
Kozubskyi Denys <kozubskiy.denis@gmail.com>
2023-11-12 18:38:20 +0200

    додав дружину Каралі

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -4,8 +4,8 @@ import data from '../../data'
 const Info = () => {
 	return (
 		<div className="Info">
-			<div>Версія: 0.2.11</div>
-			<div>Оновлено: 06.11.2023</div>
+			<div>Версія: 0.2.12</div>
+			<div>Оновлено: 12.11.2023</div>
 			<div>Всього родичів: {data.length}</div>
 		</div>
 	)
