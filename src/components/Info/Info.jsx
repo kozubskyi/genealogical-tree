144d405be9636e976a4421a7ad08d24f5583fa84
Kozubskyi Denys <kozubskiy.denis@gmail.com>
2024-01-06 00:15:48 +0200

    add yurchenko 2

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -4,8 +4,8 @@ import data from '../../data'
 const Info = () => {
 	return (
 		<div className="Info">
-			<div>Версія: 0.2.19</div>
-			<div>Оновлено: 05.01.2024</div>
+			<div>Версія: 0.2.20</div>
+			<div>Оновлено: 06.01.2024</div>
 			<div>Всього родичів: {data.length}</div>
 		</div>
 	)
