d89aefe9524e1a35e1ee7a08cc87e4c0e20c89f4
Kozubskyi Denys <kozubskiy.denis@gmail.com>
2024-01-09 14:03:05 +0200

    update

--- src/locations.js
+++ src/locations.js
@@ -15,7 +15,10 @@ const links = {
 	soshnykiv:
 		'https://www.google.com/maps/place/%D0%A1%D0%BE%D1%88%D0%BD%D0%B8%D0%BA%D0%BE%D0%B2,+%D0%9A%D0%B8%D0%B5%D0%B2%D1%81%D0%BA%D0%B0%D1%8F+%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C,+08363/@50.1063772,31.0829711,13z/data=!3m1!4b1!4m6!3m5!1s0x40d48a97dfc6351f:0x8631190bb5ebc288!8m2!3d50.1068656!4d31.1323687!16s%2Fg%2F11_yy5gzl?entry=ttu',
 	rudiakiv: 'https://uk.wikipedia.org/wiki/%D0%A0%D1%83%D0%B4%D1%8F%D0%BA%D1%96%D0%B2',
-	ovruch: 'https://www.google.com/maps/place/%D0%9E%D0%B2%D1%80%D1%83%D1%87,+%D0%96%D0%B8%D1%82%D0%BE%D0%BC%D0%B8%D1%80%D1%81%D0%BA%D0%B0%D1%8F+%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C,+11101/@51.3245729,28.7668805,13z/data=!3m1!4b1!4m6!3m5!1s0x472987f8b974aae1:0xc133be32ddf9c30a!8m2!3d51.3268314!4d28.8028968!16zL20vMDhkdmc5?authuser=0&entry=ttu',
+	ovruch:
+		'https://www.google.com/maps/place/%D0%9E%D0%B2%D1%80%D1%83%D1%87,+%D0%96%D0%B8%D1%82%D0%BE%D0%BC%D0%B8%D1%80%D1%81%D0%BA%D0%B0%D1%8F+%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C,+11101/@51.3245729,28.7668805,13z/data=!3m1!4b1!4m6!3m5!1s0x472987f8b974aae1:0xc133be32ddf9c30a!8m2!3d51.3268314!4d28.8028968!16zL20vMDhkdmc5?authuser=0&entry=ttu',
+	aarhus:
+		'https://www.google.com/maps/place/%D0%9E%D1%80%D1%85%D1%83%D1%81,+%D0%94%D0%B0%D0%BD%D0%B8%D1%8F/@56.1781721,10.0995744,12z/data=!3m1!4b1!4m6!3m5!1s0x464c4cb15397788b:0x8c4dd7d9912ea2af!8m2!3d56.162939!4d10.203921!16zL20vMGx6cA?authuser=0&entry=ttu',
 	netherlands:
 		'https://www.google.com/maps/place/%D0%9D%D0%B8%D0%B4%D0%B5%D1%80%D0%BB%D0%B0%D0%BD%D0%B4%D1%8B/@52.1869225,2.6404952,7z/data=!3m1!4b1!4m6!3m5!1s0x47c609c3db87e4bb:0xb3a175ceffbd0a9f!8m2!3d52.132633!4d5.291266!16zL20vMDU5ajI?authuser=0&entry=ttu',
 	italy:
@@ -98,6 +101,10 @@ const locations = {
 	ovruch: {
 		text: 'м. Овруч',
 		link: links.ovruch,
+	},
+	aarhus: {
+		text: 'м. Орхус',
+		link: links.aarhus,
 	},
 	netherlands: {
 		text: 'Нідерланди',
