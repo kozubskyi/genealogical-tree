1ff7ea1c304c1b21a5cab7cb556306debd8d8bd8
Kozubskyi Denys <kozubskiy.denis@gmail.com>
2024-01-21 17:34:39 +0200

    Krysanov branch update

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -4,7 +4,7 @@ import data from '../../data'
 const Info = () => {
 	return (
 		<div className="Info">
-			<div>Версія: 0.2.36</div>
+			<div>Версія: 0.2.37</div>
 			<div>Оновлено: 21.01.2024</div>
 			<div>Всього родичів: {data.length}</div>
 		</div>
