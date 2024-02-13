6df169ae493d2755296d36e42813ee8f38e6dab2
Kozubskyi Denys <kozubskiy.denis@gmail.com>
2024-02-13 02:06:22 +0200

    Stepanov update

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -4,8 +4,8 @@ import data from '../../data'
 const Info = () => {
 	return (
 		<div className="Info">
-			<div>Версія: 0.2.41</div>
-			<div>Оновлено: 11.02.2024</div>
+			<div>Версія: 0.2.42</div>
+			<div>Оновлено: 13.02.2024</div>
 			<div>Всього родичів: {data.length}</div>
 		</div>
 	)
