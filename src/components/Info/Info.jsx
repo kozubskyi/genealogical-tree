b541f35df38641385bde8e5d75e8b5ed790a2e60
Kozubskyi Denys <kozubskiy.denis@gmail.com>
2024-05-20 21:30:30 +0300

    Zhornytskyi death

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -4,8 +4,8 @@ import data from '../../data'
 const Info = () => {
 	return (
 		<div className="Info">
-			<div>Версія: 0.2.49</div>
-			<div>Оновлено: 14.05.2024</div>
+			<div>Версія: 0.2.50</div>
+			<div>Оновлено: 20.05.2024</div>
 			<div>Всього родичів: {data.length}</div>
 		</div>
 	)
